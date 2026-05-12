ecdcb545b0d15ce51a632e465b2fa927ec02d7fe
SAMALA SHREE <samala.shree2024@vitstudent.ac.in>
2026-05-12 18:54:34 +0530

    added ai-gen on homepage of socialmedia webpage

--- extension/content.js
+++ extension/content.js
@@ -1,1 +1,28 @@
-alert("SynthShield is running!");
+function addLabel() {
+  if (!document.querySelector(".synthshield-label")) {
+    const label = document.createElement("div");
+    label.innerText = "⚠ Likely AI-generated";
+    label.className = "synthshield-label";
+
+   label.style.position = "fixed";
+label.style.top = "80px";
+label.style.right = "20px";
+label.style.left = "auto";
+    label.style.backgroundColor = "yellow";
+    label.style.color = "black";
+    label.style.padding = "8px";
+    label.style.fontSize = "12px";
+    label.style.fontWeight = "bold";
+    label.style.zIndex = "999999";
+
+    document.body.appendChild(label);
+  }
+}
+
+const observer = new MutationObserver(addLabel);
+observer.observe(document.body, {
+  childList: true,
+  subtree: true
+});
+
+addLabel();
